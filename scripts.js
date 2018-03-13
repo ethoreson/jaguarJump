a0e255d4c0abf5bd8e7da1ca87c91fedf2c1bfbb
Emilie Thoreson <thoresonemilie@gmail.com>
2018-03-13 16:32:15 -0700

    github pages commit

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$("#submitButton").click(function(event) {
-		var sue_total = 0;
-		var claire_total = 0;
+		var kiawna_total = 0;
+		var sam_total = 0;
 		var c_total = 0;
 		var d_total = 0;
 
@@ -20,9 +20,9 @@ $(document).ready(function() {
 		$("thanksMessage").fadeToggle();
 
 		if (formQ1 === "answer1") {
-			sue_total += 1;
+			kiawna_total += 1;
 		} else if (formQ1 === "answer2") {
-			claire_total += 1;
+			sam_total += 1;
 		} else if (formQ1 === "answer3") {
 			c_total += 1;
 		} else {
@@ -30,13 +30,13 @@ $(document).ready(function() {
 		}
 
 		if (formQ2 === "answer1") {
-			claire_total += 1;
+			sam_total += 1;
 		} else if (formQ2 === "answer2") {
 			c_total += 1;
 		} else if (formQ2 === "answer3") {
 			d_total += 1;
 		} else {
-			sue_total += 1;
+			kiawna_total += 1;
 		}
 
 		if (formQ3 === "answer1") {
@@ -44,29 +44,29 @@ $(document).ready(function() {
 		} else if (formQ3 === "answer2") {
 			d_total += 1;
 		} else if (formQ3 === "answer3") {
-			sue_total += 1;
+			kiawna_total += 1;
 		} else {
-			claire_total += 1;
+			sam_total += 1;
 		}
 
 		if (formQ4 === "answer1") {
 			d_total += 1;
 		} else if (formQ4 === "answer2") {
-			sue_total += 1;
+			kiawna_total += 1;
 		} else if (formQ4 === "answer3") {
-			claire_total += 1;
+			sam_total += 1;
 		} else {
 			c_total += 1;
 		}
 
-		console.log(sue_total);
-		console.log(claire_total);
+		console.log(kiawna_total);
+		console.log(sam_total);
 		console.log(c_total);
 		console.log(d_total);
 
-		if ((sue_total > claire_total) && (sue_total > c_total) && (sue_total > d_total)) {
+		if ((kiawna_total > sam_total) && (kiawna_total > c_total) && (kiawna_total > d_total)) {
 			$("#a").fadeToggle();
-		} else if ((claire_total > c_total) && (claire_total > d_total)) {
+		} else if ((sam_total > c_total) && (sam_total > d_total)) {
 			$("#b").fadeToggle();
 		} else if ((c_total > d_total)) {
 			$("#c").fadeToggle();
